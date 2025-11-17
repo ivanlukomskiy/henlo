2442333fc6b8d088432105021f78fa7cd52bbf22
ivanlukomskiy <ivanlukomskiy@avride.ai>
2025-11-17 20:21:21 +0400

    vite, mantine, nanostores, pwa

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
+
+export default defineConfig({
+  base: '/henlo/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      includeAssets: [
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png'
+      ],
+      manifest: {
+        name: 'henlo',
+        short_name: 'henlo',
+        description: 'app to learn new vocabulary',
+        theme_color: '#111827',
+        background_color: '#111827',
+        display: 'standalone',
+        scope: '/henlo/',
+        start_url: '/henlo/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}'
+        ]
+      }
+    })
+  ]
+})
